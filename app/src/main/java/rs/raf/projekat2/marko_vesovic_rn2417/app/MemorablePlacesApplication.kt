47d085a6c5c3c0033f6c39a321bc035cf19c5d6f
Marko <mvesovic17@raf.rs>
2020-06-16 19:21:23 +0200

    Initial setup

---
+++ app/src/main/java/rs/raf/projekat2/marko_vesovic_rn2417/app/MemorablePlacesApplication.kt
@@ -0,0 +1,7 @@
+package rs.raf.projekat2.marko_vesovic_rn2417.app
+
+import android.app.Application
+
+class MemorablePlacesApplication: Application() {
+
+}
---
+++ app/src/main/java/rs/raf/projekat2/marko_vesovic_rn2417/module/core_module.kt
@@ -0,0 +1,52 @@
+package rs.raf.projekat2.marko_vesovic_rn2417.module
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+
+val coreModule = module {
+    single<SharedPreferences> {
+        androidApplication().getSharedPreferences(androidApplication().packageName, Context.MODE_PRIVATE)
+    }
+
+    single { createRetrofit(moshi = get(), httpClient = get()) }
+
+    single { createMoshi() }
+
+    single { createOkHttpClient() }
+}
+
+fun createMoshi(): Moshi {
+    return Moshi.Builder().add(Date::class.java, Rfc3339DateJsonAdapter()).build()
+}
+
+fun createRetrofit(moshi: Moshi, httpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("URL")
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi).asLenient())
+        .client(httpClient)
+        .build()
+}
+
+fun createOkHttpClient(): OkHttpClient {
+    val httpClient = OkHttpClient.Builder()
+    httpClient.readTimeout(60, TimeUnit.SECONDS)
+    httpClient.connectTimeout(60, TimeUnit.SECONDS)
+    httpClient.writeTimeout(60, TimeUnit.SECONDS)
+    return httpClient.build()
+}
+
+inline fun <reified T> create(retrofit: Retrofit): T {
+    return retrofit.create<T>(T::class.java)
+}
